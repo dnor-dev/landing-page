a3a569cecc5331f5bb227cc903d5b20cdd96f010
Francaolisa <francaolisa@gmail.com>
2023-12-29 20:23:13 +0100

    added image

--- src/components/home/SpunkySale.tsx
+++ src/components/home/SpunkySale.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import Image from "next/image";
-import presaleimg from "@/assets/img/banner/pres-image.svg";
+import presaleimg from "@/assets/img/banner/pres-image.png";
 
 const SpunkySale = () => {
   const [isCopied, setIsCopied] = useState(false);
